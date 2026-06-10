1bc553eee277321e429409045764a307ee490c0f
Artur <asr.engsoft@gmail.com>
2026-06-10 18:00:12 -0300

    Refactor DB inserts

--- data/collector/collector.py
+++ data/collector/collector.py
@@ -9,10 +9,11 @@ BASE_URL = "https://api.brawlstars.com/v1"
 TOKEN = os.getenv("BRAWL_API_TOKEN")
 PLAYER_TAG = os.getenv("PLAYER_TAG")
 
+# headers é necessário para autenticação na API do Brawl Stars, usando o token de acesso
 headers = {
     "Authorization": f"Bearer {TOKEN}"
 }
-
+# faz uma solicitação GET para obter o log de batalhas de um jogador específico, usando o PLAYER_TAG e os headers para autenticação
 def fetch_battlelog():
     url = f"{BASE_URL}/players/{PLAYER_TAG}/battlelog"
     response = requests.get(url, headers=headers)
@@ -23,6 +24,7 @@ def fetch_battlelog():
 
     return response.json()
 
+# salva os dados de uma batalha específica no banco de dados, usando a conexão fornecida e os detalhes da batalha
 def save_match(conn, battle):
     cur = conn.cursor()
 
@@ -30,35 +32,30 @@ def save_match(conn, battle):
     event = battle["event"]
     mode = event["mode"]
     map_name = event["map"]
-
     result = battle["battle"].get("result")
     duration = battle["battle"].get("duration")
 
     # inserir match
-    cur.execute("""
+    query = """
         INSERT OR IGNORE INTO matches (battle_time, mode, map, duration, result)
         VALUES (?, ?, ?, ?, ?)
-    """, (battle_time, mode, map_name, duration, result))
+    """
+    dados_partida = (battle_time, mode, map_name, duration, result)
+    cur.execute(query, dados_partida)
 
     # pegar teams
     teams = battle["battle"].get("teams", [])
-
     for team_index, team in enumerate(teams):
         for player in team:
             brawler = player["brawler"]
 
-            cur.execute("""
-                INSERT INTO match_players (
-                    battle_time, team, brawler, power, trophies
-                )
+            query = ("""
+                INSERT INTO match_players (battle_time, team, brawler, power, trophies)
                 VALUES (?, ?, ?, ?, ?)
-            """, (
-                battle_time,
-                team_index,
-                brawler["name"],
-                brawler.get("power"),
-                brawler.get("trophies")
-            ))
+            """)
+            dados_jogador = (battle_time, team_index, brawler["name"], brawler["power"], player["trophies"])
+            cur.execute(query, dados_jogador)
 
+    # salvar as alterações no banco de dados 
     conn.commit()
 
